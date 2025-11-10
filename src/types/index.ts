73767a98fe3e3a4d9f72134d2f81278293a68ed3
Kibreab Solomon <kbieman21@yahoo.com>
2025-11-10 13:00:09 -0700

    Handling errors related to optional properties

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,7 +7,7 @@ export interface Task {
   description?: string;
   priority: TaskPriority;
   status: TaskStatus;
-  dueDate?: string;
+  dueDate: string;
   createdAt?: string;
 }
 
--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -1,4 +1,20 @@
-export const sortTasks = (tasks, sortBy) => {
+
+// export const sortTasks = (tasks: any[], sortBy: string) => {
+//   switch (sortBy) {
+//     case 'priority':
+//       const priorityOrder = { high: 3, medium: 2, low: 1 };
+//       return [...tasks].sort((a, b) => priorityOrder[b.priority] - priorityOrder[a.priority]);
+//     case 'dueDate':
+//       return [...tasks].sort((a, b) => new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime());
+//     default:
+//       return tasks;
+//   }
+// };
+
+import type { Task } from "../types";
+
+
+export const sortTasks = (tasks:Task[], sortBy:string): Task[] => {
   switch (sortBy) {
     case 'priority':
       const priorityOrder = { high: 3, medium: 2, low: 1 };
